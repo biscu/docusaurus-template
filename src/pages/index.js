0e4ae6fb09e356af3d5dfcf11fda05fa488b2ec6
davidebiscuso <davide.biscuso@easypark.net>
2024-11-25 20:31:18 +0100

    added motion.dev

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
+import { motion } from "motion/react";
 import Button from "../components/Button/button";
 import Heading from "@theme/Heading";
 import MockLight from "../../static/img/header-mock-light.png";
@@ -15,7 +16,9 @@ export default function Home() {
       description="Description will go into a meta tag in <head />"
     >
       <div className="text-3xl flex justify-center items-center h-screen w-screen">
-        Homepage
+        <motion.h1 initial={{ opacity: 0 }} animate={{ y: -50, opacity: 1 }}>
+          Homepage
+        </motion.h1>
       </div>
     </Layout>
   );
