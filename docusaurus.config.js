5fc7da67a23c734535e8cedf588832a177721eb4
davidebiscuso <biscu@live.it>
2024-12-12 09:56:00 +0100

    og imag fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -117,7 +117,7 @@ const config = {
         respectPrefersColorScheme: false,
       },
       // Replace with your project's social card
-      image: "img/og_image.jpg",
+      image: "./img/og_image.jpg",
       navbar: {
         title: "",
         logo: {
