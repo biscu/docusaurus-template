82d3b701fee7ebfce30de1848d243bbdb6579d96
davidebiscuso <biscu@live.it>
2024-12-14 22:40:07 +0100

    added audio to voice over

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -1,12 +1,41 @@
-import ReactHowler from 'react-howler'
+import Howler from "react-howler";
+import { useState } from "react";
+import { Play, Pause } from "lucide-react"; 
 
 const Audio = () => {
-    return (
-      <ReactHowler
-        src='http://goldfirestudios.com/proj/howlerjs/sound.ogg'
-        playing={true}
-      />
-    )
-}
+    const [playing, setPlaying] = useState(false);
+    const [howlerRef, setHowlerRef] = useState(null); 
   
-export default Audio
+    const toggleAudio = () => {
+      if (playing) {
+        howlerRef.seek(0); 
+        setPlaying(false); 
+      } else {
+        setPlaying(true); 
+      }
+    };
+  
+    const handleEnd = () => {
+      setPlaying(false);
+    };
+
+  return (
+    <div className="flex justify-start items-center p-4 rounded-full bg-neutral-900">
+      <Howler
+        src="/audio/voice-over-button.m4a"
+        playing={playing}
+        onEnd={handleEnd} 
+        ref={(ref) => setHowlerRef(ref)} 
+      />
+      <button
+        onClick={toggleAudio} 
+        className="flex flex-row items-center px-4 py-2 rounded-full border-none cursor-pointer text-neutral-900 bg-neutral-100"
+      >
+        {playing ? <Pause className="mr-2" /> : <Play className="mr-2" />}
+        {playing ? "Stop" : "Play"} 
+      </button>
+    </div>
+  );
+};
+
+export default Audio;
