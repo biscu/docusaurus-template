928006bbaeebd1323cee01f53f8ae0c1dde7e1d9
davidebiscuso <biscu@live.it>
2024-12-16 13:56:49 +0100

    added header to preloader

--- src/components/Preload.js
+++ src/components/Preload.js
@@ -8,6 +8,8 @@ const Preload = () => {
         <div className="hidden">
             <img src={useBaseUrl("./img/placeholder_cover_dark.svg")} alt="preload" />
             <img src={useBaseUrl("./img/placeholder_cover_light.svg")} alt="preload" />
+            <img src={useBaseUrl("./img/header.svg")} alt="preload" />
+            <img src={useBaseUrl("./img/header2.svg")} alt="preload" />
         </div>
     );
 }
