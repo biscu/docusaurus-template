3528ee4cb75adb1a9f25bfca69a80bfafabc9479
davidebiscuso <biscu@live.it>
2024-12-16 01:49:04 +0100

    home page design

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,14 +12,48 @@ export default function Home() {
       description="Description will go into a meta tag in <head />"
     >
       <Preload />
-      <div className="flex items-center justify-center w-[700px] h-screen mx-auto text-center text-balance	">
+      <div className="flex flex-col items-center justify-center max-w-[700px] h-full py-48 mx-auto text-center text-balance	gap-6">
+        <div className="flex mb-4">
+          <motion.img 
+          initial={{ y: 30, x: 5, rotateZ: -10, opacity: 0 }}
+          animate={{ y: 4, opacity: 1 }} 
+          transition={{ duration: 0.4, delay: 0.2, ease: "easeInOut" }}
+          drag
+          dragConstraints={{ left: -2, right: 2, top: -2, bottom: 2 }}
+          src="/img/Stamp1.png" alt="Postnord logo" width="88px" height="88px"
+          className="cursor-grab" />
+          <motion.img 
+          initial={{ y: 30, opacity: 0 }}
+          animate={{ y: 2, opacity: 1 }}  
+          transition={{ duration: 0.4, delay: 0.3, ease: "easeInOut" }}
+          drag
+          dragConstraints={{ left: -2, right: 2, top: -2, bottom: 2 }}
+          className="cursor-grab"
+          src="/img/Stamp2.png" alt="Postnord logo" width="88px" height="88px" />
+          <motion.img 
+          initial={{ y: 30, x: -10, rotateZ: 5, opacity: 0 }}
+          animate={{ y: 10, opacity: 1 }} 
+          transition={{ duration: 0.4, delay: 0.4, ease: "easeInOut" }}
+          drag
+          dragConstraints={{ left: -2, right: 2, top: -2, bottom: 2 }}
+          className="cursor-grab"
+          src="/img/Stamp3.png" alt="Postnord logo" width="88px" height="88px" />
+        </div>
         <motion.h1
-          initial={{ opacity: 0 }}
-          animate={{ y: -30, opacity: 1 }}
+          initial={{ y: 30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
           className="text-6xl"
         >
           Postnord app source of truth
         </motion.h1>
+        <motion.p
+          initial={{ y: 30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.2, delay: 0.2, ease: "easeInOut" }}
+          className="text-2xl dark:text-neutral-500 text-neutral-400"
+        >
+        The one place of information for design, usability, accessibility and strategy.
+        </motion.p>
       </div>
     </Layout>
   );
